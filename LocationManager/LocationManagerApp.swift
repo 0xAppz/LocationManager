3b139cdeff035f25c277c8e8bc1110c1c6319a73
Rav4ppz <s550robert@icloud.com>
2022-04-01 23:22:25 -0700

    Initial Commit

---
+++ LocationManager/LocationManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationManagerApp.swift
+//  LocationManager
+//
+//  Created by Robert C on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct LocationManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
